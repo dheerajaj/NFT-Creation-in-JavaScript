6f4559083367d32ea540b9e4f21b3e1fd21f0a4d
Dheeraj Tripathi <71077984+dheerajaj@users.noreply.github.com>
2023-08-18 17:41:50 +0530

    Add files via upload

--- CreateWallet.js
+++ CreateWallet.js
@@ -1,17 +1,18 @@
-const { Wallet, Client } = require("xrpl");
-const ECDSA = require("xrpl/dist/npm/ECDSA");
+const { ECDSA, Wallet, Client } = require('xrpl');
 
 async function createWallet() {
     let newWallet = Wallet.generate(ECDSA.ed25519);
     console.log(newWallet);
 }
+
 async function fundWallet() {
-    let wallet = Wallet.fromSeed("sEdTcpfZ5CVqm6sxc86ZVmhjsE4L3GM");
-    let client = new Client("wss://s.altnet.rippletest.net/");
-    await client.connect();
-    console.log("are we connected? " + client.isConnected());
-    let result = await client.fundWallet(wallet);
+    let wallet = Wallet.fromSeed("sEdVN9qzC34BndJGbgfXAJc7G4fPX2e");
+    let xrplClient = new Client("wss://s.altnet.rippletest.net:51233"); // Use wss instead of was for WebSocket connection
+    await xrplClient.connect();
+    console.log("Are we connected? " + xrplClient.isConnected());
+    let result = await xrplClient.fundWallet(wallet);
     console.log(result);
 }
-//createWallet();
+
+createWallet();
 fundWallet();
--- CreateWallet.ts
+++ CreateWallet.ts
@@ -1,19 +1,19 @@
-import { Wallet,Client } from "xrpl";
-import ECDSA from "xrpl/dist/npm/ECDSA";
+import{ECDSA, Wallet, Client }from 'xrpl';
 
-async function CreateWallet() {
+
+async function createWallet() {
     let newWallet = Wallet.generate(ECDSA.ed25519)
     console.log(newWallet);
+    
 }
-    async function fundWallet() {
-        let wallet = Wallet.fromSeed("sEd716b5piTG9dvBmUMAyvcQiUXUcAK");
-        let client = new Client("was://s.altnet.rippletest.net/");
-        await client.connect();
-        console.log("are we connected? " +client.isConnected());
-        let result = await client.fundWallet(wallet);
-        console.log(result);
-        }
-       // CreateWallet();
-     fundWallet();
 
-   
+async function fundWallet() {
+    let wallet = Wallet.fromSeed("sEd716b5piTG9dvBmUMAyvcQiUXUcAK");
+    let client = new Client("was://s.altnet.rippletest.net/");
+    await client.connect();
+    console.log("are we connected? " +client.isConnected());
+    let result = await client.fundWallet(wallet);
+    console.log(result);
+    }
+
+    createWallet();
